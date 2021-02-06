ec7e3aa417e96e7683dd015ceff2b221d0bdc548
danielg-r <d4nielg.r@gmail.com>
2021-02-05 23:00:54 -0500

    Assets importados de nuevo
    - DoTweenPro
    - Events 2.0

---
+++ Assets/Scripts/scriptbasura.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptbasura : MonoBehaviour
+{
+    public void Test (Holi jeje) {
+        if (jeje == Holi.RamirezGay) print("En efecto");
+    }
+}
+
+public enum Holi {
+    RamirezPuto,
+    RamirezPincheGordo,
+    RamirezGay
+}
