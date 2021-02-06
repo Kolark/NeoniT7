5009007e24a53b22aecc89830a437702fe72fc48
danielg-r <d4nielg.r@gmail.com>
2021-02-05 23:17:20 -0500

    Importados scripts del player

---
+++ Assets/Scripts/Player (Scripts)/BasicCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicCharacter : MonoBehaviour
+{
+    CharacterMovement character;
+    private void Awake()
+    {
+        character = GetComponent<CharacterMovement>();
+    }
+
+    private void FixedUpdate()
+    {
+        character.Move();
+        character.Crouch();
+    }
+}
