a9a174d9489ca55ecb0542e1616f4ff991e4a6e3
ZeritFreeman <juanc.sierram@upb.edu.co>
2021-02-17 21:57:43 -0500

    Behaviours y animaciones de ataque
    Se modfico el script de BasicCharacter, Se agregaron las animaciones place holders del ataque y se crearon 3 behaviours por animacion

--- Assets/Scripts/Player (Scripts)/BasicCharacter.cs
+++ Assets/Scripts/Player (Scripts)/BasicCharacter.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class BasicCharacter : MonoBehaviour
 {
+    private static BasicCharacter instance;
+    public static BasicCharacter Instance { get => instance; }
+
     CharacterMovement character;
     InputController inputController;
     [SerializeField] LayerMask hurtBoxEnemy;
     [SerializeField] Transform pos;
+
+    public bool canReceiveInput;
+    public bool inputReceived;
+
     private void Awake()
     {
+        canReceiveInput = true;
+        inputReceived = false;
+
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        instance = this;
+
         character = GetComponent<CharacterMovement>();
         inputController = InputController.Instance;
         inputController.Jump += character.Jump;
@@ -25,14 +41,30 @@ public class BasicCharacter : MonoBehaviour
     public void Attack()
     {
         Debug.Log("Attack");
-        Collider2D Hit = Physics2D.OverlapPoint(pos.position, hurtBoxEnemy);
-        IEnemyHurtBox enemy = Hit?.GetComponent<IEnemyHurtBox>();
-        enemy?.OnReceiveDamage();
+        if (canReceiveInput)
+        {
+            inputReceived = true;
+            canReceiveInput = false;
+
+            Collider2D Hit = Physics2D.OverlapPoint(pos.position, hurtBoxEnemy);
+            IEnemyHurtBox enemy = Hit?.GetComponent<IEnemyHurtBox>();
+            enemy?.OnReceiveDamage();
+        }
+        else
+        {
+            return;
+        }
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(pos.position, 0.25f);
+    }
+
+    public void AnimatorInputManager()
+    {
+        if (!canReceiveInput) canReceiveInput = true;
+        else canReceiveInput = false;
     }
 }
