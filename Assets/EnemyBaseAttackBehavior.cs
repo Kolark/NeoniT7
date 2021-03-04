768c6ad2ea23a4aa0a43d16b7e4043d4e1747789
Kolark <47009873+Kolark@users.noreply.github.com>
2021-03-04 03:47:28 -0500

    Se junto todo en el nivel 1 para poder generar la build

--- Assets/EnemyBaseAttackBehavior.cs
+++ Assets/EnemyBaseAttackBehavior.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class EnemyBaseAttackBehavior : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        EnemyController enemy = animator.GetComponentInParent<EnemyController>();       
-    }
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+              
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -19,6 +19,7 @@ public class EnemyBaseAttackBehavior : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-       
+        EnemyController enemy = animator.GetComponentInParent<EnemyController>();
+        enemy.Ai.onAttackEnd();
     }
 }
