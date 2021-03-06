6b0ae7b6a2c7e409f5b1fd2ae72d6ffb9b8c1c16
JuanCalvache <47004448+JuanCalvache@users.noreply.github.com>
2021-03-06 14:07:20 -0500

    Trampa A implementada
    Se Creo el Script TrampA que ya le quita vida al enemigo.
    No tiene estetica, solo place Holders

---
+++ Assets/Scripts/Tramps/TrampA.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrampA : MonoBehaviour
+{
+    [SerializeField] Transform pos;
+    [SerializeField] Vector3 size;
+    [SerializeField] LayerMask layer;
+    [SerializeField] float releseTime;
+    [SerializeField] SpriteRenderer col;
+    Color previousCol;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Player")
+        {
+            
+            Invoke("ActiveTramp", releseTime);
+        }
+    }
+
+    public void ActiveTramp()
+    {
+        Collider2D Hit = Physics2D.OverlapBox(pos.position, size, 0f, layer);
+        PlayerDamageHandler player = Hit?.GetComponent<PlayerDamageHandler>();
+        player?.OnReceiveDamage();
+        previousCol = col.color;
+        col.color = Color.red;
+        Invoke("Release", releseTime);
+    }
+
+    public void Release()
+    {
+        col.color = previousCol;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(pos.position, size);
+    }
+}
