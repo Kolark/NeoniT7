e522474aaf6945a21937429e3351be1b139ccb47
JuanCalvache <47004448+JuanCalvache@users.noreply.github.com>
2021-03-18 11:36:57 -0500

    Death Zone
    El personje muere si se cae a una fosa

---
+++ Assets/Scripts/Tramps/DeathZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+    [SerializeField] Vector3 size;
+    [SerializeField] LayerMask layer;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Collider2D Hit = Physics2D.OverlapBox(transform.position, size, 0f, layer);
+            PlayerDamageHandler player = Hit?.GetComponent<PlayerDamageHandler>();
+            player?.Death();
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, size);
+    }
+
+}
