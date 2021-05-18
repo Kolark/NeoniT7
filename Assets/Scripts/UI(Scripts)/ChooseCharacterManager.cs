3f4e7a553b386a44c18708434a49669ac4378bfe
Kolark <47009873+Kolark@users.noreply.github.com>
2021-05-18 03:53:45 -0500

    Revert "Añadí Descripción de personajes en la pantalla que se escogen."
    This reverts commit ed885ad7930e4404501098d30fc7450f7188cfd6.

--- Assets/Scripts/UI(Scripts)/ChooseCharacterManager.cs
+++ Assets/Scripts/UI(Scripts)/ChooseCharacterManager.cs
@@ -9,7 +9,7 @@ public class ChooseCharacterManager : MonoBehaviour
     public static ChooseCharacterManager Instance { get => instance;}
     [SerializeField] Image currentCharacter;
     [SerializeField] Sprite[] characters;
-    [SerializeField] RectTransform[] characterInfo;
+
     private void Awake()
     {
         if(instance == null)
@@ -26,11 +26,6 @@ public class ChooseCharacterManager : MonoBehaviour
     public void ChangeVisual(CharacterType type)
     {
         currentCharacter.sprite = characters[(int)type];
-        for (int i = 0; i < characterInfo.Length; i++)
-        {
-            characterInfo[i].gameObject.SetActive(false);
-        }
-        characterInfo[(int)type].gameObject.SetActive(true);
         Debug.Log("Changevisual: " + type.ToString());
     }
 
